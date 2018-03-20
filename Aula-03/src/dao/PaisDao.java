2fd550bb9bc4d06ec9f7fba14f828b5473e15dcb
rafael <rafael2014barros@outlook.com>
2018-03-20 18:31:17 -0300

    Aula_03

---
+++ Aula-03/src/dao/PaisDao.java
@@ -0,0 +1,90 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import model.Pais;
+
+public class PaisDao {
+	public int criar(Pais pais) {
+		String sqlInsert = "INSERT INTO cliente(nome, fone, email) VALUES (?, ?, ?)";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlInsert);) {
+			stm.setString(1, pais.getNome());
+			stm.setString(2, pais.getFone());
+			stm.setString(3, pais.getEmail());
+			stm.execute();
+			String sqlQuery = "SELECT LAST_INSERT_ID()";
+			try (PreparedStatement stm2 = conn.prepareStatement(sqlQuery);
+					ResultSet rs = stm2.executeQuery();) {
+				if (rs.next()) {
+					pais.setId(rs.getInt(1));
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return pais.getId();
+	}
+
+	public void atualizar(Pais pais) {
+		String sqlUpdate = "UPDATE cliente SET nome=?, fone=?, email=? WHERE id=?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlUpdate);) {
+			stm.setString(1, pais.getNome());
+			stm.setString(2, pais.getFone());
+			stm.setString(3, pais.getEmail());
+			stm.setInt(4, pais.getId());
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void excluir(int id) {
+		String sqlDelete = "DELETE FROM cliente WHERE id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlDelete);) {
+			stm.setInt(1, id);
+			stm.execute();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Pais carregar(int id) {
+		Pais cliente = new Pais();
+		cliente.setId(id);
+		String sqlSelect = "SELECT nome, fone, email FROM cliente WHERE cliente.id = ?";
+		// usando o try with resources do Java 7, que fecha o que abriu
+		try (Connection conn = ConnectionFactory.obtemConexao();
+				PreparedStatement stm = conn.prepareStatement(sqlSelect);) {
+			stm.setInt(1, cliente.getId());
+			try (ResultSet rs = stm.executeQuery();) {
+				if (rs.next()) {
+					cliente.setNome(rs.getString("nome"));
+					cliente.setFone(rs.getString("fone"));
+					cliente.setEmail(rs.getString("email"));
+				} else {
+					cliente.setId(-1);
+					cliente.setNome(null);
+					cliente.setFone(null);
+					cliente.setEmail(null);
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		} catch (SQLException e1) {
+			System.out.print(e1.getStackTrace());
+		}
+		return cliente;
+	}
+
+}
